a71a0eed6c0ece72b240e96f5383c75beca1edbf
HetMistri <232153644+HetMistri@users.noreply.github.com>
2025-11-11 21:59:30 +0530

    Feat: Added vercel speed and analytics insights

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,8 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import CustomCursor from "../components/CustomCursor/CustomCursor";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Lazy load routes for better performance
 const Visualizer = lazy(() => import("../features/visualizer/Visualizer"));
@@ -20,11 +22,14 @@ export default function App() {
         <Routes>
           <Route path="/home" element={<Landing />} />
           {/* <Route path="/visualizer" element={<Visualizer />} /> */}
-          {/* Momentarily Switched to Visualizer as default landing */} <Route path="/" element={<Visualizer />} />
+          {/* Momentarily Switched to Visualizer as default landing */}{" "}
+          <Route path="/" element={<Visualizer />} />
           <Route path="/app" element={<Navigate to="/visualizer" replace />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
+      <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   );
 }
